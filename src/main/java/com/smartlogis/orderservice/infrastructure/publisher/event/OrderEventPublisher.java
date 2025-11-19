5decc8aa3a654b92aa0d07d484454a15d156d5b4
hoonssac <Hoonssac.dev@gmail.com>
2025-11-19 14:26:51 +0900

    feat: 주문 이벤트 퍼블리셔 작성 #3

---
+++ src/main/java/com/smartlogis/orderservice/infrastructure/publisher/event/OrderEventPublisher.java
@@ -0,0 +1,23 @@
+package com.smartlogis.orderservice.infrastructure.publisher.event;
+
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.stereotype.Component;
+
+import com.smartlogis.orderservice.domain.event.OrderCreatedEvent;
+import com.smartlogis.orderservice.infrastructure.config.RabbitMQConfig;
+
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class OrderEventPublisher {
+	private final RabbitTemplate rabbitTemplate;
+
+	public void publishOrderCreated(OrderCreatedEvent event) {
+		rabbitTemplate.convertAndSend(
+			RabbitMQConfig.ORDER_CREATED_EXCHANGE,
+			RabbitMQConfig.ORDER_CREATED_ROUTING_KEY,
+			event
+		);
+	}
+}
