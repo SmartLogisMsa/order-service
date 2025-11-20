efd63a0c0287eac23159426fbc46470a8782fe10
hoonssac <Hoonssac.dev@gmail.com>
2025-11-20 13:42:58 +0900

    feat: 주문 상태 추가 (배송 중, 배송 완료) #3

--- src/main/java/com/smartlogis/orderservice/domain/entity/OrderStatus.java
+++ src/main/java/com/smartlogis/orderservice/domain/entity/OrderStatus.java
@@ -2,7 +2,9 @@ package com.smartlogis.orderservice.domain.entity;
 
 public enum OrderStatus {
 	CREATED("접수됨"),
-	CANCELED("취소됨");
+	CANCELED("취소됨"),
+	SHIPPED("배송중"),
+	DELIVERED("배송완료");
 
 	private final String description;
 
