9ff4b643baefbbb76b1d192b8ce38d88dc0faa2c
hoonssac <Hoonssac.dev@gmail.com>
2025-11-21 17:49:49 +0900

    refactor: 주문 취소 http method 변경 #7

--- src/main/java/com/smartlogis/orderservice/interfaces/controller/OrderController.java
+++ src/main/java/com/smartlogis/orderservice/interfaces/controller/OrderController.java
@@ -8,6 +8,7 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -47,7 +48,7 @@ public class OrderController {
 		return ResponseEntity.status(HttpStatus.CREATED).body(ApiResponse.successWithDataOnly(response));
 	}
 
-	@DeleteMapping("/cancel/{orderId}")
+	@PatchMapping("/cancel/{orderId}")
 	@PreAuthorize("hasAnyRole('MASTER', 'COMPANY_MANAGER')")
 	@Operation(
 		summary = "주문 취소",
