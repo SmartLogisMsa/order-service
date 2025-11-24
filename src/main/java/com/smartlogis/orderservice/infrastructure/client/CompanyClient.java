73298f41e54bdb0d6f4c83a533f3f649c2f0d977
hoonssac <Hoonssac.dev@gmail.com>
2025-11-24 13:04:07 +0900

    feat: 업체 조회 FeignClient 추가 #5

---
+++ src/main/java/com/smartlogis/orderservice/infrastructure/client/CompanyClient.java
@@ -0,0 +1,17 @@
+package com.smartlogis.orderservice.infrastructure.client;
+
+import java.util.UUID;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.smartlogis.common.presentation.ApiResponse;
+import com.smartlogis.orderservice.infrastructure.client.dto.CompanyResponse;
+
+@FeignClient(name = "company-service")
+public interface CompanyClient {
+
+	@GetMapping("/v1/companies/{id}")
+	ApiResponse<CompanyResponse> getCompany(@PathVariable("id") UUID id);
+}
