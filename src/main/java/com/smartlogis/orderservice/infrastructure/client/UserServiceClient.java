50e897411e1eebbaebc15a3747b825bf349c0ee4
hoonssac <Hoonssac.dev@gmail.com>
2025-11-25 11:25:59 +0900

    fix: user-service 클라이언트 주소 수정 #9

--- src/main/java/com/smartlogis/orderservice/infrastructure/client/UserServiceClient.java
+++ src/main/java/com/smartlogis/orderservice/infrastructure/client/UserServiceClient.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import com.smartlogis.common.presentation.ApiResponse;
 import com.smartlogis.orderservice.infrastructure.client.dto.UserInfoResponse;
 
-@FeignClient(name = "localhost:8082")
+@FeignClient(name = "user-service")
 public interface UserServiceClient {
 
 	@GetMapping("/v1/users")
