d93759ea102982698610134ffe3dcf7d36c0f472
hoonssac <Hoonssac.dev@gmail.com>
2025-11-26 13:52:44 +0900

    fix: Swagger 제목 수정 #11

--- src/main/java/com/smartlogis/orderservice/infrastructure/config/SwaggerConfig.java
+++ src/main/java/com/smartlogis/orderservice/infrastructure/config/SwaggerConfig.java
@@ -29,7 +29,7 @@ public class SwaggerConfig {
 			.servers(List.of(new Server().url(url)))
 			.components(new Components().addSecuritySchemes("Bearer", new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")))
 			.addSecurityItem(new SecurityRequirement().addList("Bearer"))
-			.info(new Info().title("스파르타 허브")
+			.info(new Info().title("주문 서비스")
 				.description("Order API"));
 	}
 
